82496fc15465418292616fac93225852e3567730
glock901 <110033510+glock901@users.noreply.github.com>
2020-05-17 07:04:50 +0000

    Update Request.php

--- Crypto/Core/Components/CoinImage/CoinLogoView.swift
+++ Crypto/Core/Components/CoinImage/CoinLogoView.swift
@@ -20,3 +20,26 @@ struct CoinLogoView: View {
                 .font(.headline)
                 .foregroundColor(Color.theme.accent)
                 .lineLimit(1)
+                .minimumScaleFactor(0.5)
+            Text(coin.name)
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+struct CoinLogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+        CoinLogoView(coin: dev.coin)
+            .previewLayout(.sizeThatFits)
+
+            CoinLogoView(coin: dev.coin)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
