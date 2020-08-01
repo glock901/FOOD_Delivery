3ef3c85c7e697bb82c57d99aaf1f8cf1185b90b8
glock901 <110033510+glock901@users.noreply.github.com>
2020-08-01 06:26:41 +0000

    add missing headers

--- Crypto/Core/Components/StatisticView.swift
+++ Crypto/Core/Components/StatisticView.swift
@@ -11,4 +11,5 @@ struct StatisticView: View {
     let stat: StatisticModel
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
-   
+            Text(stat.title)
+              
