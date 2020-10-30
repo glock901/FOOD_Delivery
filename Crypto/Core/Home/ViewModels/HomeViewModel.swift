dabbd5a71d5ef8fbfb64b6ffe22c2b354b821a1f
glock901 <110033510+glock901@users.noreply.github.com>
2020-10-30 13:58:31 +0000

    PyFBA

--- Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -44,4 +44,6 @@ class HomeViewModel: ObservableObject {
     }
 
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
-      
+        guard !text.isEmpty else { return coins }
+
+  
