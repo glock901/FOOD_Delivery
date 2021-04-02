2c46d3763cd883599dae81d9963ce16227a9a4a2
glock901 <110033510+glock901@users.noreply.github.com>
2021-04-02 07:52:01 +0000

    envelop

--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -19,4 +19,5 @@ struct HomeView: View {
                 .ignoresSafeArea()
                 .sheet(isPresented: $showPortfilioView) {
                     PortfolioView()
-                
+                        .environmentObject(vm)
+          
