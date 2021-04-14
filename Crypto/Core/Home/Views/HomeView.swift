02c3eef161b72444c6a68c42e75896c32c887ecd
glock901 <110033510+glock901@users.noreply.github.com>
2021-04-14 10:28:38 +0000

    v0.12-preview.120.18+252

--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -33,4 +33,5 @@ struct HomeView: View {
                 }
 
                 if showPortfilio {
-                    portfolioCoinsL
+                    portfolioCoinsList
+                        .transition(.mov
