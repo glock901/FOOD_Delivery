4e50aa2b1b4fbcbbf1738dd8749364e3af7f1429
glock901 <110033510+glock901@users.noreply.github.com>
2022-03-13 14:30:13 +0000

    layout adjustments

--- Crypto/Models/CoinModel.swift
+++ Crypto/Models/CoinModel.swift
@@ -9,4 +9,6 @@ import Foundation
 // CoinGecko API
 /*
  URL:
- https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&
+ https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+
+ JSON Respon
