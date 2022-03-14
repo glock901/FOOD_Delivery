b6a900196b7f67e4b46950a146b6cd9db03a270b
glock901 <110033510+glock901@users.noreply.github.com>
2022-03-14 12:52:53 +0000

    Add git aliases

--- Crypto/Models/CoinModel.swift
+++ Crypto/Models/CoinModel.swift
@@ -11,4 +11,8 @@ import Foundation
  URL:
  https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
 
- JSON Respon
+ JSON Response:
+
+ {
+ "id": "bitcoin",
+ "symbol
