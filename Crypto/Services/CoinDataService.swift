bc693e3f95453097d9280ecb8412fff531f8d0e3
glock901 <110033510+glock901@users.noreply.github.com>
2022-07-28 14:39:41 +0000

    RDMA/cxgb4: Use dst parameter in import_ep()

--- Crypto/Services/CoinDataService.swift
+++ Crypto/Services/CoinDataService.swift
@@ -19,4 +19,5 @@ class CoinDataService {
     }
 
     private func getcoins() {
-        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=t
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
+ 
