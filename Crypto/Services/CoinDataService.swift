0ff8918588de342b49ea2927f9cac3a05c432fd5
glock901 <110033510+glock901@users.noreply.github.com>
2022-07-28 19:43:53 +0000

    Add build status.

--- Crypto/Services/CoinDataService.swift
+++ Crypto/Services/CoinDataService.swift
@@ -22,4 +22,5 @@ class CoinDataService {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
         else { return }
 
-        coinSubscrition = Netw
+        coinSubscrition = NetworkingManager.download(url: url)
+            .
