64fedff87187badab491e270c7cf3e0a22d84178
glock901 <110033510+glock901@users.noreply.github.com>
2022-10-16 16:52:36 +0000

    version 3.0

--- Crypto/Services/MarketDataService.swift
+++ Crypto/Services/MarketDataService.swift
@@ -3,3 +3,30 @@
 //  MarketDataService.swift
 //  Crypto
 //
+//  Created by Jonni Akesson on 2022-08-13.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+
+    @Published var marketData: MarketDataModel? = nil
+
+    private var marketDataSubscrition: AnyCancellable?
+
+    init() {
+        getData()
+    }
+
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+
+        marketDataSubscrition = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedGlobalData in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscrition?.cancel()
+            })
+    }
+}
