89e2b1087932505237e7ecaed7511759e50e5817
glock901 <110033510+glock901@users.noreply.github.com>
2022-12-18 14:12:47 +0000

    Prepare 1.3.4 release

--- Crypto/Utilities/LocalFileManager.swift
+++ Crypto/Utilities/LocalFileManager.swift
@@ -48,4 +48,9 @@ class LocalFileManager {
             do {
                 try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
             } catch let error {
-                print("error creating directory. Folder name: \(folderNam
+                print("error creating directory. Folder name: \(folderName) \(error)")
+            }
+        }
+    }
+
+   
