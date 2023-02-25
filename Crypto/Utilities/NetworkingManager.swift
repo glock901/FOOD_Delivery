bd1d93b7945fc306ee47bf5f3cfef21401600cd6
glock901 <110033510+glock901@users.noreply.github.com>
2023-02-25 11:40:53 +0000

    Fixed test cases

--- Crypto/Utilities/NetworkingManager.swift
+++ Crypto/Utilities/NetworkingManager.swift
@@ -25,4 +25,5 @@ class NetworkingManager {
 
     static func download(url: URL) -> AnyPublisher<Data, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: Disp
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMa
